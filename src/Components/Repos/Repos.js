7080937ba8056fcdd7538a0cd6d1053c5c55cf0a
Timmyctt <timmyc1990@yahoo.com>
2023-05-11 12:45:42 +0100

    added

--- src/Components/Repos/Repos.js
+++ src/Components/Repos/Repos.js
@@ -1,57 +1,51 @@
-import { Link } from 'react-router-dom'
-import { useState, useEffect} from 'react'
-import './Repos.css'
+import { useParams, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./Repos.css";
 
-const initialFormData = {
-    github: ''
-}
+export default function Repos() {
+  const params = useParams();
+  const initialFormData = {
+    github: `${params.username}`,
+  };
+  const [repos, setRepos] = useState([]);
+  const [username, setUsername] = useState(`${params.username}`);
+  const [formData, setFormData] = useState(initialFormData);
+  const [notFound, setNotFound] = useState(false);
 
-export default function Repos () {
-    const [repos, setRepos] = useState([])
-    const [username, setUsername] = useState('')
-    const [formData, setFormData] = useState(initialFormData)
-    const [notFound, setNotFound] = useState(false)
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${username}/repos`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message === "Not Found") {
+          setNotFound(true);
+        } else {
+          setNotFound(false);
+          setRepos(data);
+          console.log(data);
+        }
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${username}/repos`)
-        .then(res => res.json())
-        .then(data => {
-            if (data.message === 'Not Found') {
-                setNotFound(true)
-            } else {
-                setNotFound(false)
-                setRepos(data)
-                console.log(data)
-            }
-        })
-    }, [username])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUsername(formData.github);
+  };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, github: e.target.value });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setUsername(formData.github)
-    }
-
-    const handleChange = (e) => {
-        setFormData({...formData, github: e.target.value})
-    }
-
-    return (
-        <>
-            {
-                notFound && <div>user '{username}' does not exist</div>
-            }
-            <form onSubmit={handleSubmit}>
-                <input type='text' name='github' onChange={handleChange} value={formData.github}/>
-                <button>Get Repos</button>
-            </form>
-            {
-                repos.map((repo => (
-                    <div>
-                        <Link className={'repoLink'} to={`/${username}/${repo.name}`}>{repo.name}</Link>
-                    </div>
-                )))
-            }
-        </>
-    )
+  return (
+    <>
+      {notFound && <div>user '{username}' does not exist</div>}
+      {!notFound && <h4>{username}'s Repos</h4>}
+      {repos.map((repo) => (
+        <div>
+          <Link className={"repoLink"} to={`/${username}/${repo.name}`}>
+            {repo.name}
+          </Link>
+        </div>
+      ))}
+    </>
+  );
 }
