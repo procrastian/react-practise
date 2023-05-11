c51d5d0470255853c9427365ddb6a9b132f267a4
Ian Grantham <dev.iangrantham@gmail.com>
2023-05-11 15:37:24 +0100

    user page correctly renders

--- src/Components/Repos/Repos.js
+++ src/Components/Repos/Repos.js
@@ -4,42 +4,38 @@ import "./Repos.css";
 
 export default function Repos() {
   const params = useParams();
-  const initialFormData = {
-    github: `${params.username}`,
-  };
   const [repos, setRepos] = useState([]);
-  const [username, setUsername] = useState(`${params.username}`);
-  const [formData, setFormData] = useState(initialFormData);
+  const [username] = useState(`${params.username}`);
   const [notFound, setNotFound] = useState(false);
 
   useEffect(() => {
     fetch(`https://api.github.com/users/${username}/repos`)
       .then((res) => res.json())
       .then((data) => {
+        console.log('data', data)
+        console.log('data[0]', data[0])
+        console.log('owner', data[0].owner)
+        console.log('avatar_url', data[0].owner.avatar_url)
         if (data.message === "Not Found") {
           setNotFound(true);
         } else {
           setNotFound(false);
           setRepos(data);
-          console.log(data);
         }
-      });
-  }, []);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setUsername(formData.github);
-  };
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, github: e.target.value });
-  };
+      })
+  }, [])
 
   return (
     <>
       {notFound && <div>user '{username}' does not exist</div>}
-      {!notFound && <h4>{username}'s Repos</h4>}
+
+      {!notFound && repos.length > 0 && (
+        <>
+         <h4>{username}'s Repos</h4>
       <img alt={"avatar"} src={`${repos[0].owner.avatar_url}`} />
+      </>
+      )}
+
       {repos.map((repo) => (
         <div>
           <Link className={"repoLink"} to={`/${username}/${repo.name}`}>
