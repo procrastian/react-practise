74fefdafb5dd790d878b76131dbc40da530e13ab
carolina arruda <carolinacalouroarruda@gmail.com>
2023-05-14 14:11:33 +0200

    finished ext 4

--- src/Components/Repos/AllNotes.js
+++ src/Components/Repos/AllNotes.js
@@ -3,32 +3,67 @@ import { useState, useEffect } from 'react'
 export default function AllNotes () {
 
     const [notes, setNotes] = useState([]);
+    const [search, setSearch] = useState('')
+    const [filteredNotes, setFilteredNotes] = useState([]);
+
 
     useEffect(() => {
         fetch("http://localhost:4000/notes")
           .then((res) => res.json())
-          .then((data) => setNotes(data))
+          .then((data) => {
+            setNotes(data);
+            setFilteredNotes(data)})
+
 
       }, []);
 
+      const handleSubmit =(e) => {
+        e.preventDefault()
+      }
 
+      const handleSearch = (event) => {
+        const searchTerm = event.target.value;
+        setSearch(searchTerm);
+
+        if (searchTerm.length === 0) {
+            setFilteredNotes(notes);
+        } else {
+           const filteredNotes = notes.filter((note) => {
+                return note.comment.toLowerCase().includes(searchTerm.toLowerCase());
+            });
+            setFilteredNotes(filteredNotes);
+        }
+
+    }
     return (
         <>
-            <h1>Welcome to all the saucy notes...</h1>
+            <form autoComplete='off' onSubmit={handleSubmit}>
+                <label htmlFor='search-notes' id='search'>
+                    Search notes
+                </label>
+                <input
+                    placeholder='type to search'
+                    id='search-notes'
+                    name='search-notes'
+                    type='text'
+                    value={search}
+                    onChange={handleSearch}
+                />
+                <input type='submit' value={'search'} />
+            </form>
             <ul>
-                {notes.map((note) => (
-                <li>
-                    user: {note.username} -- 
-                    <br/>
-                    repo: {note.name}
-                    <br/>
-                    {note.comment}
-                    <hr/>
-
-                </li>
-            ))}
+                {filteredNotes.map((note) => (
+                    <li key={note.id}>
+                        user: {note.username} --
+                        <br />
+                        repo: {note.name}
+                        <br />
+                        {note.comment}
+                        <hr />
+                    </li>
+                ))}
             </ul>
-            
         </>
-    )
+    );
 }
+
